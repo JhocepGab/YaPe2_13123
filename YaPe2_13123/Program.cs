97cdf9991c0257b24fd5a0c92adb6b0f6a05d37e
Josegit <N00034975@upn.pe>
2025-04-23 17:01:28 -0500

    Agregar archivos de proyecto.

---
+++ YaPe2_13123/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaPe2_13123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el nùmero de elementos: ");
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+                if (i > 1) ;
+
+                    }
+           
+        }
+    }
+}
